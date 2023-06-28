b6e1f6a4f7091d573ecae6049f0b332ded0472c8
Phongle1311 <hoaiphong13.11.2002@gmail.com>
2023-06-28 10:14:09 +0700

    delete all

--- front-end/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const BASE_URL = 'http://localhost:5000' // URL of the Flask server
-
-export const summarizeText = async (text: string) => {
-  const response = await axios.post(`${BASE_URL}/api/summarize`, { text })
-  return response.data
-}
